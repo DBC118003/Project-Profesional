3866dbfa7a660fbdd4867d18369f36ca1071f021
DBC118003 <vannysianipar999@gmail.com>
2021-05-03 19:34:08 +0700

    Database Website

---
+++ database-grafik-webiste-umkm.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 03, 2021 at 02:30 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `grafik_pada_website_umkm_pky`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+-- Error reading structure for table grafik_pada_website_umkm_pky.admin: #1932 - Table 'grafik_pada_website_umkm_pky.admin' doesn't exist in engine
+-- Error reading data for table grafik_pada_website_umkm_pky.admin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `grafik_pada_website_umkm_pky`.`admin`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+-- Error reading structure for table grafik_pada_website_umkm_pky.barang: #1932 - Table 'grafik_pada_website_umkm_pky.barang' doesn't exist in engine
+-- Error reading data for table grafik_pada_website_umkm_pky.barang: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `grafik_pada_website_umkm_pky`.`barang`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cod`
+--
+-- Error reading structure for table grafik_pada_website_umkm_pky.cod: #1932 - Table 'grafik_pada_website_umkm_pky.cod' doesn't exist in engine
+-- Error reading data for table grafik_pada_website_umkm_pky.cod: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `grafik_pada_website_umkm_pky`.`cod`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_orderan`
+--
+-- Error reading structure for table grafik_pada_website_umkm_pky.detail_orderan: #1932 - Table 'grafik_pada_website_umkm_pky.detail_orderan' doesn't exist in engine
+-- Error reading data for table grafik_pada_website_umkm_pky.detail_orderan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `grafik_pada_website_umkm_pky`.`detail_orderan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kriteria`
+--
+-- Error reading structure for table grafik_pada_website_umkm_pky.kriteria: #1932 - Table 'grafik_pada_website_umkm_pky.kriteria' doesn't exist in engine
+-- Error reading data for table grafik_pada_website_umkm_pky.kriteria: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `grafik_pada_website_umkm_pky`.`kriteria`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kurir`
+--
+-- Error reading structure for table grafik_pada_website_umkm_pky.kurir: #1932 - Table 'grafik_pada_website_umkm_pky.kurir' doesn't exist in engine
+-- Error reading data for table grafik_pada_website_umkm_pky.kurir: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `grafik_pada_website_umkm_pky`.`kurir`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_gateway`
+--
+-- Error reading structure for table grafik_pada_website_umkm_pky.payment_gateway: #1932 - Table 'grafik_pada_website_umkm_pky.payment_gateway' doesn't exist in engine
+-- Error reading data for table grafik_pada_website_umkm_pky.payment_gateway: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `grafik_pada_website_umkm_pky`.`payment_gateway`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+-- Error reading structure for table grafik_pada_website_umkm_pky.pelanggan: #1932 - Table 'grafik_pada_website_umkm_pky.pelanggan' doesn't exist in engine
+-- Error reading data for table grafik_pada_website_umkm_pky.pelanggan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `grafik_pada_website_umkm_pky`.`pelanggan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+-- Error reading structure for table grafik_pada_website_umkm_pky.pembayaran: #1932 - Table 'grafik_pada_website_umkm_pky.pembayaran' doesn't exist in engine
+-- Error reading data for table grafik_pada_website_umkm_pky.pembayaran: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `grafik_pada_website_umkm_pky`.`pembayaran`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemesanan_barang`
+--
+-- Error reading structure for table grafik_pada_website_umkm_pky.pemesanan_barang: #1932 - Table 'grafik_pada_website_umkm_pky.pemesanan_barang' doesn't exist in engine
+-- Error reading data for table grafik_pada_website_umkm_pky.pemesanan_barang: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `grafik_pada_website_umkm_pky`.`pemesanan_barang`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengiriman`
+--
+-- Error reading structure for table grafik_pada_website_umkm_pky.pengiriman: #1932 - Table 'grafik_pada_website_umkm_pky.pengiriman' doesn't exist in engine
+-- Error reading data for table grafik_pada_website_umkm_pky.pengiriman: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `grafik_pada_website_umkm_pky`.`pengiriman`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `umkm`
+--
+-- Error reading structure for table grafik_pada_website_umkm_pky.umkm: #1932 - Table 'grafik_pada_website_umkm_pky.umkm' doesn't exist in engine
+-- Error reading data for table grafik_pada_website_umkm_pky.umkm: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `grafik_pada_website_umkm_pky`.`umkm`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
